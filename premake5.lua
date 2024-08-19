ce586c9966d3e14a42fbf8bcd9c92a8a866e272d
Berke <berkepite@hotmail.com>
2024-08-19 15:49:57 +0300

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,20 @@
+project "GLFW"
+     kind "StaticLib"
+     language "C"
+     targetdir (GLFW .. bin .. "/%{cfg.buildcfg}/")
+     systemversion "latest"
+     staticruntime "On"
+
+     files
+	   {
+  		 ("%{wks.location}" .. ENGINE .. "/vendor/GLFW/glfw3.h"),
+	  	 ("%{wks.location}" .. ENGINE .. "/vendor/GLFW/glfw3native.h")
+     }
+
+     filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+  
+     filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
