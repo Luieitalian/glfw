ea572bfd4499fd1c96e666fa2701d1e177d23ef0
Luieitalian <enderbud125@hotmail.com>
2024-08-21 14:43:01 +0300

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,51 @@
+project "GLFW"
+    kind "StaticLib"
+    language "C"
+    cdialect "C99"
+    targetdir "%{wks.location}/GLFW/bin/%{cfg.buildcfg}/"
+
+    objdir "%{wks.location}/GLFW/obj/%{cfg.buildcfg}/"
+
+    includedirs {
+      "include"
+    }
+
+    files
+    {
+      "include/GLFW/glfw3.h",
+      "include/GLFW/glfw3native.h",
+      "src/glfw_config.h",
+      "src/context.c",
+      "src/init.c",
+      "src/input.c",
+      "src/monitor.c",
+      "src/vulkan.c",
+      "src/window.c"
+    }
+
+    filter "system:windows"
+        defines { "_GLFW_WIN32" }
+        links { "gdi32" }
+        systemversion "latest"
+        staticruntime "On"
+
+        files
+        {
+          "src/win32_init.c",
+          "src/win32_joystick.c",
+          "src/win32_monitor.c",
+          "src/win32_time.c",
+          "src/win32_thread.c",
+          "src/win32_window.c",
+          "src/wgl_context.c",
+          "src/egl_context.c",
+          "src/osmesa_context.c"
+        }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
