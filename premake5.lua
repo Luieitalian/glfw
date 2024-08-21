df9ead646846ed9450a44ce4849d33e2c769f78d
Luieitalian <enderbud125@hotmail.com>
2024-08-21 15:35:15 +0300

    fix script

--- premake5.lua
+++ premake5.lua
@@ -2,25 +2,27 @@ project "GLFW"
     kind "StaticLib"
     language "C"
     cdialect "C99"
+    location "%{wks.location}/GLFW"
     targetdir "%{wks.location}/GLFW/bin/%{cfg.buildcfg}/"
 
     objdir "%{wks.location}/GLFW/obj/%{cfg.buildcfg}/"
-
-    includedirs {
-      "include"
-    }
 
     files
     {
       "include/GLFW/glfw3.h",
       "include/GLFW/glfw3native.h",
       "src/glfw_config.h",
-      "src/context.c",
       "src/init.c",
+      "src/context.c",
       "src/input.c",
-      "src/monitor.c",
       "src/vulkan.c",
-      "src/window.c"
+      "src/monitor.c",
+      "src/window.c",
+      "src/platform.c",
+      "src/null_monitor.c",
+      "src/null_window.c",
+      "src/null_init.c",
+      "src/null_joystick.c"
     }
 
     filter "system:windows"
@@ -34,6 +36,7 @@ project "GLFW"
           "src/win32_init.c",
           "src/win32_joystick.c",
           "src/win32_monitor.c",
+          "src/win32_module.c",
           "src/win32_time.c",
           "src/win32_thread.c",
           "src/win32_window.c",
